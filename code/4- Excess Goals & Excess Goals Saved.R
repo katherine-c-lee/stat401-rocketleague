ee23e82df9d53417e5321345c308eb5ab118612a
lindswang <76761384+lindswang@users.noreply.github.com>
2022-03-14 15:31:31 -0400

    Update 4- Excess Goals & Excess Goals Saved.R

--- code/4- Excess Goals & Excess Goals Saved.R
+++ code/4- Excess Goals & Excess Goals Saved.R
@@ -8,12 +8,14 @@ library(randomForest)  # random forests
 library(gbm)           # boosting
 library(RColorBrewer)
 library(ROCR)
+library(dplyr)
 
 
 ############################ Load data from file 4 ###########################
 setwd('/Users/lindawang/Library/Mobile Documents/com~apple~CloudDocs/Linda Wang/Spring 2022 Classes/STAT 401/Final Project/stat401-rocketleague/code')
 
 data_with_boost_xg <- read.csv(file = "data/clean/data_with_boost_xg.csv")
+shot_data <- read.csv(file = "data/clean/shot_data.csv")
 
 ################################# Excess Goals  #################################
 # this section is basically katie's code in the 3- file
@@ -68,16 +70,46 @@ data_with_boost_xg %>%
   filter(shot_taker_id == "76561198028093603")
 
 ################################# Excess Goals Saved #################################
-xg_saved <- data_with_boost_xg %>%
-  group_by(shot_taker_id) %>%
-  summarise(sum_xg = sum(xg))
+## inner join with shot_data to grab opponent IDs
+shot_data_ids <- shot_data %>%
+  select(contains("id"), "ball_pos_x", "ball_pos_y", "ball_pos_z","goal",contains("arc_cos")) %>%
+  select(-c("idx"))
 
-xg_saved <- data_with_boost_xg %>%
-  inner_join(total_goals, by = "shot_taker_id") %>%
-  inner_join(total_attempts, by = "shot_taker_id") %>%
-  mutate(outperformance = total_goals/(sum_xg)) %>%
-  mutate(actual_goal_rate = total_goals/count) %>%
-  mutate(avg_xg = sum_xg/count) %>%
-  arrange(desc(outperformance)) %>%
-  filter(count>20)
+boost_data_ids <- data_with_boost_xg %>%
+  inner_join(shot_data_ids, by = c("shot_taker_id","ball_pos_x", "ball_pos_z","goal",))
+
+nrow(boost_data_ids) - nrow(data_with_boost_xg) # deal with this later
+
+xg_saved_1 <- boost_data_ids %>%
+  rename("saver_id" = "opp_1_id") %>%
+  mutate(defended_xg = xg - goal) %>%
+  group_by(saver_id) %>%
+  summarise(total_defended = sum(defended_xg),
+            count = n()) %>%
+  ungroup() %>%
+  select(c("saver_id","total_defended","count"))
+
+xg_saved_2 <- boost_data_ids %>%
+  rename("saver_id" = "opp_2_id") %>%
+  mutate(defended_xg = xg - goal) %>%
+  group_by(saver_id) %>%
+  summarise(total_defended = sum(defended_xg),
+            count = n()) %>%
+  ungroup() %>%
+  select(c("saver_id","total_defended","count"))
+
+unique_ids <- data_with_boost_xg %>%
+  group_by(data_with_boost_xg$shot_taker_id) %>%
+  summarise(count = n()) %>%
+  filter(count>20) %>%
+  rename("saver_id" = "data_with_boost_xg$shot_taker_id")
+
+xg_saved <- unique_ids %>%
+  inner_join(xg_saved_1, by = "saver_id") %>%
+  inner_join(xg_saved_2, by = "saver_id") %>%
+  mutate(total_defended = total_defended.x + total_defended.x) %>%
+  mutate(total_count = count.x + count.y) %>%
+  mutate(excess_goals_saved = total_defended / total_count) %>%
+  select(c("saver_id", "excess_goals_saved"))
+
 
